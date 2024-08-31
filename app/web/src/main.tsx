83d8c657b25674f6c3197ece60302bdaf363aa2b
BirushaNdegeya <birushandegeya@gmail.com>
2024-08-31 16:48:28 +0200

    add shadcn ui

--- app/web/src/main.tsx
+++ app/web/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import './globals.css';
+import { Button } from './components/ui/button';
+import { Bell } from 'lucide-react';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return (
+    <div className='p-4'>
+      <h1 className='text-primary mb-1'>PIMA UJUZI</h1>
+
+      <Button size="lg">
+        Default Button <Bell className='ml-2' />
+      </Button>
+
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById('root')!)
+root.render(
+  <App />
+);
