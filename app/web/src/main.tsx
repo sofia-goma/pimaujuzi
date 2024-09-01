ae5694c8597015e7d3b93f13f85e48d4e42b7aed
BirushaNdegeya <birushandegeya@gmail.com>
2024-09-01 11:36:23 +0200

    add ligth and dark mode switch

--- app/web/src/main.tsx
+++ app/web/src/main.tsx
@@ -1,22 +1,41 @@
 import { createRoot } from 'react-dom/client';
-import './globals.css';
 import { Button } from './components/ui/button';
 import { Bell } from 'lucide-react';
+import { ThemeProvider } from './providers/theme-provider';
+import { ModeToggle } from './components/mode-toggle';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import DashboardLayout from './components/layouts/dashboard-layout';
+import './globals.css';
 
 function App() {
+  const message: IMessage = { message: "Hello world!" }
+  console.log(message);
   return (
-    <div className='p-4'>
-      <h1 className='text-primary mb-1'>PIMA UJUZI</h1>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
 
-      <Button size="lg">
-        Default Button <Bell className='ml-2' />
-      </Button>
+      <div className='p-4'>
+        <ModeToggle />
+        <h1 className='text-primary mb-1'>PIMA UJUZI</h1>
 
-    </div>
+        <Button size="lg">
+          Default Button <Bell className='ml-2' />
+        </Button>
+
+      </div>
+    </ThemeProvider>
   );
 }
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <DashboardLayout />
+  }
+]);
+
 const root = createRoot(document.getElementById('root')!)
 root.render(
-  <App />
+  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+    <RouterProvider router={router} />
+  </ThemeProvider>
 );
