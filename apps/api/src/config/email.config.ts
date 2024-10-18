2124c3f2c1340ea5007c939537ea688e06715211
blessingtutka <blessingtutka298@gmail.com>
2024-10-18 21:02:37 +0200

    chore(config): rename to email config

--- apps/api/src/config/email.config.ts
+++ apps/api/src/config/email.config.ts
@@ -1,15 +1,15 @@
-interface MailConfig {
+interface EmailConfig {
   host: string;
   user: string;
   port: number;
   password: string;
 }
 
-const mailConfig: MailConfig = {
+const emailConfig: EmailConfig = {
   host: process.env.EMAIL_HOST as string,
   user: process.env.EMAIL_HOST_USER as string,
   port: parseInt(process.env.MY_PORT || '587', 10),
   password: process.env.EMAIL_HOST_PASSWORD as string,
 };
 
-export default mailConfig;
+export default emailConfig;
