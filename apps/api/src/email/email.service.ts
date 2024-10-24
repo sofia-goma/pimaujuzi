c0e043b754105dee22ac19496f548bd43e76b14a
blessingtutka <blessingtutka298@gmail.com>
2024-10-24 21:14:21 +0200

    fix(email): use the correct mailer

--- apps/api/src/email/email.service.ts
+++ apps/api/src/email/email.service.ts
@@ -1,53 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
-import * as hbs from 'nodemailer-express-handlebars';
-import emailConfig from 'src/config/email.config';
-import * as path from 'path';
-import { Transporter } from 'nodemailer';
+import { MailerService } from '@nestjs-modules/mailer';
 import { addMinutes, differenceInMinutes } from 'date-fns';
+import emailConfig from 'src/config/email.config';
 
 @Injectable()
 export class EmailService {
-  private transporter: Transporter;
-
-  constructor(private configService: typeof emailConfig) {
-    this.transporter = nodemailer.createTransport({
-      host: this.configService.host,
-      port: this.configService.port,
-      secure: false,
-      auth: {
-        user: this.configService.user,
-        pass: this.configService.password,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-
-    const hbsConfig = {
-      viewEngine: {
-        extName: '.hbs',
-        partialsDir: path.resolve(__dirname, '../email/templates'),
-        layoutsDir: path.resolve(__dirname, '../email/templates'),
-        defaultLayout: '',
-      },
-      viewPath: path.resolve(__dirname, '../email/templates'),
-      extName: '.hbs',
-    };
-
-    this.transporter.use('compile', hbs(hbsConfig));
-  }
+  constructor(private readonly mailerService: MailerService) {}
 
   async sendMail(options) {
     const mailOptions = {
-      from: this.configService.user,
       to: options.to,
+      from: emailConfig.user,
       subject: options.subject,
       template: options.template,
       context: options.context,
     };
 
-    await this.transporter.sendMail(mailOptions);
+    await this.mailerService.sendMail(mailOptions);
   }
   async sendOtpEmail(
     email: string,
@@ -60,7 +29,7 @@ export class EmailService {
       const mailOptions = {
         to: email,
         subject: 'One Time Password',
-        template: 'send-otp',
+        template: './send-otp',
         context: {
           firstName,
           otpCode,
